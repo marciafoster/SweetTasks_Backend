b7e172acc26eed43c66523b6881e93adb1351221
marciafoster <mfoster9tre@gmail.com>
2023-11-19 10:58:42 -0500

    created tabe in schema file

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+drop database if exists tasks_dev;
+CREATe DATABASE tasks_dev;
+
+
+\c tasks_dev;
+
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    task_name TEXT NOT NULL,
+    description TEXT,
+    assigned_to VARCHAR,
+    due_date DATE,
+    is_complete BOOLEAN,
+    priority INTEGER);
